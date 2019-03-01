2047093a0d69766f8aa534ac648e27964680afb0
carly <carly@noukod.com>
2019-03-01 17:51:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function logDriverNames(drivers){
+    drivers.forEach(function(el){
+        console.log(el.name);
+    });
+}
+
+function logDriversByHometown(drivers, location){
+    drivers.forEach(function(el,i,arr){
+        if(el.hometown === location){
+            console.log(el.name);
+        }
+    });
+}
+
+function driversByRevenue(drivers){
+    return [...drivers].sort(function (a,b){
+        return a.revenue - b.revenue;
+    });
+}
+
+function driversByName(drivers){
+    return [...drivers].sort(function(a,b){
+        return a.name.localeCompare(b.name);
+    });
+}
+
+function totalRevenue(drivers){
+    return drivers.reduce(function (acc, el){
+        return acc + el.revenue;
+    },0)
+}
+
+function averageRevenue(drivers){
+    return totalRevenue(drivers) / drivers.length;
+}
